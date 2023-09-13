ff3b83e070be8e2990b02c64d0d26b683584295c
Ivan test <curtees1@gmail.com>
2023-09-13 10:12:04 +0700

    change BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int a, double b){
-        double result =( a / (b * b));
+    public int calculate(int mass, double height) {
+        double result = (mass / (height * height));
         return (int) result;
 
     }
